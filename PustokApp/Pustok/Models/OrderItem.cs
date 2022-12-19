1e1772ca958d1103f273387b2daba651618ed9bb
TAHiRLI <tahrli.tahirr@gmail.com>
2022-12-19 22:22:13 +0400

    first Commit

---
+++ PustokApp/Pustok/Models/OrderItem.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pustok.Models
+{
+    public class OrderItem
+    {
+
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public int OrderId { get; set; }
+        public int Count { get; set; }
+
+        [Column(TypeName = "money")]
+        public decimal ItemCostPrice { get; set; }
+
+        [Column(TypeName = "money")]
+        public decimal ItemSalePrice { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal ItemDiscountPercent { get; set; }
+
+        public Order Order { get; set; }
+        public Book Book { get; set; }
+    }
+}
